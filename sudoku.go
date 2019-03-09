4a01df85d7c5c3040770169400e7ae2a2efeb926
Jani Hakala <jjhakala@gmail.com>
2019-03-09 10:20:04 +0200

    Return error if grid has invalid size

--- sudoku.go
+++ sudoku.go
@@ -17,7 +17,11 @@ package main
 
 import "fmt"
 
-func printGrid(grid string) {
+func printGrid(grid string) error {
+	if len(grid) != 81 {
+		return fmt.Errorf("Grid '%s' has invalid size", grid)
+	}
+
 	for i, c := range(grid) {
 		fmt.Printf("%c", c)
 		if (i + 1) % 9 == 0 {
@@ -26,10 +30,15 @@ func printGrid(grid string) {
 			fmt.Print(" ")
 		}
 	}
+
+	return nil
 }
 
 func main() {
 	var grid1 string = "700600008800030000090000310006740005005806900400092100087000020000060009600008001"
 
-	printGrid(grid1)
+	err := printGrid(grid1)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
 }
