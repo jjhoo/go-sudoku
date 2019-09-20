ae410ef6393904ccf4b9ead6054817a0e802425b
Jani Hakala <jjhakala@gmail.com>
2019-09-21 00:01:04 +0300

    Add tests

--- sudoku_test.go
+++ sudoku_test.go
@@ -59,3 +59,63 @@ func TestGrid4(t *testing.T) {
 	}
 	s.PrintGrid()
 }
+
+func TestBadInput1(t *testing.T) {
+	// Test invalid characters in input
+	grid := "CAT21003009000060000000500080403006007000800500700040003000000020000700800195000"
+
+	s := Sudoku{}
+
+	err := s.initGrid(grid)
+	if err == nil {
+		t.Error(fmt.Errorf("initGrid was expected to fail"))
+	}
+}
+
+func TestBadInput2(t *testing.T) {
+	// Test short input
+	grid := "0092100300900006000000050008040300600700080050070004000300000002000070080019500"
+
+	s := Sudoku{}
+
+	err := s.initGrid(grid)
+	if err == nil {
+		t.Error(fmt.Errorf("initGrid was expected to fail"))
+	}
+}
+
+func TestGridString(t *testing.T) {
+	grid := "000921003009000060000000500080403006007000800500700040003000000020000700800195000"
+
+	s := Sudoku{}
+
+	err := s.initGrid(grid)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ngrid := s.GetGridString()
+
+	if grid != ngrid {
+		t.Error(fmt.Errorf("GetGridString returned different string"))
+	}
+}
+
+func TestPrintGrid1(t *testing.T) {
+	grid := "000921003009000060000000500080403006007000800500700040003000000020000700800195000"
+
+	err := printGrid(grid)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPrintGrid2(t *testing.T) {
+	// Short grid length
+	grid := "00092100300900006000000050008040300600700080050070004000300000002000070080019500"
+
+	err := printGrid(grid)
+	if err == nil {
+		t.Error(err)
+	}
+}
